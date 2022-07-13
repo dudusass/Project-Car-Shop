59d067042e533e1e8385680ef579ff0788d6cef9
Sass <dudu_sass12@hotmail.com>
2022-07-13 17:45:13 -0300

    test car controller

---
+++ src/tests/unit/controllers/carController.test.ts
@@ -0,0 +1,141 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Request, Response } from 'express';
+
+import CarController from '../../../controllers/CarController';
+import { Car } from '../../../interfaces/CarInterface';
+import { carExemplo, carResposta, carsListaResposta, carRespostaAtualizada } from '../mocks/mocks';
+
+describe('[CAR CONTROLLER] => TESTANDO CONTROLLER', () => {
+  const carController = new CarController();
+
+  describe(':: VERIFICANDO ACESSO A ROTA /CARS', () => {
+    it(':: VERIFICANDO ROTA /CARS', async () => {
+      const result = carController.route;
+      expect(result).to.be.equal('/cars');
+    });
+  });
+});
+
+describe('TESTING CREATE ROUTE', () => {
+  const carController = new CarController();
+  const request = {} as Request<Car>;
+  const response = {} as Response;
+
+  describe('CREATE CONTROLLER', () => {
+    before(async() => {
+      request.body = carExemplo;
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns(response);
+      sinon.stub(carController.service, 'create').resolves(carResposta);
+    });
+    after(() => sinon.restore());
+    it('EXPECTS STATUS 201 TO RETURN', async() => {
+      await carController.create(request, response);
+      expect((response.status as sinon.SinonStub).calledWith(201)).to.be.equal(true);
+    });
+
+    it('EXPECTS TO RETURN AN OBJECT', async() => {
+      await carController.create(request, response);
+      expect((response.json as sinon.SinonStub).calledWith(carResposta)).to.be.equal(true);
+    });
+  });
+});
+
+describe('TESTING ROUTE READ', () => {
+  const carController = new CarController();
+  const request = {} as Request<{ id: string; }>;
+  const response = {} as Response;
+  
+  describe('CONTROLLER READ', () => {
+    before(async() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns(response);
+      sinon.stub(carController.service, 'read').resolves(carsListaResposta);
+    });
+    after(() => sinon.restore());
+
+    it('EXPECTS STATUS 200 TO RETURN', async() => {
+      await carController.read(request, response);
+      expect((response.status as sinon.SinonStub).calledWith(200)).to.be.equal(true);
+    });
+
+    it('EXPECTS TO RETURN AN OBJECT', async() => {
+      await carController.read(request, response);
+      expect((response.json as sinon.SinonStub).calledWith(carsListaResposta)).to.be.equal(true);
+    });
+  });
+});
+
+describe('TESTING ROUTE READONE', () => {
+  const carController = new CarController();
+  const request = {} as Request<{ id: string; }>;
+  const response = {} as Response;
+  
+  describe('CONTROLLER READONE', () => {
+    before(async() => {
+      request.params = { id: carResposta._id };
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns(response);
+      sinon.stub(carController.service, 'readOne').resolves(carResposta);
+    });
+    after(() => sinon.restore());
+
+    it('EXPECTS STATUS 200 TO RETURN', async() => {
+      await carController.readOne(request, response);
+      expect((response.status as sinon.SinonStub).calledWith(200)).to.be.equal(true);
+    });
+
+    it('EXPECTS TO RETURN AN OBJECT', async() => {
+      await carController.readOne(request, response);
+      expect((response.json as sinon.SinonStub).calledWith(carResposta)).to.be.equal(true);
+    });
+  });
+});
+
+describe('TESTING ROUTE UPDATE', () => {
+  const carController = new CarController();
+  const request = {} as Request<{ id: string; }>;
+  const response = {} as Response;
+  
+  describe('CONTROLLER UPDATE', () => {
+    before(async() => {
+      request.params = { id: carRespostaAtualizada._id };
+      request.body = carRespostaAtualizada;
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns(response);
+      sinon.stub(carController.service, 'update').resolves(carRespostaAtualizada);
+    });
+    after(() => sinon.restore());
+
+    it('EXPECTS STATUS 200 TO RETURN', async() => {
+      await carController.update(request, response);
+      expect((response.status as sinon.SinonStub).calledWith(200)).to.be.equal(true);
+    });
+
+    it('EXPECTS TO RETURN AN OBJECT', async() => {
+      await carController.update(request, response);
+      expect((response.json as sinon.SinonStub).calledWith(carRespostaAtualizada)).to.be.equal(true);
+    });
+  });
+});
+
+describe('TESTING ROUTE DELETE', () => {
+  const carController = new CarController();
+  const request = {} as Request<{ id: string; }>;
+  const response = {} as Response;
+  
+  describe('CONTROLLER DELETE', () => {
+    before(async() => {
+      request.params = { id: carResposta._id };
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns(response);
+      sinon.stub(carController.service, 'delete').resolves(carResposta);
+    });
+    after(() => sinon.restore());
+    it('EXPECTS STATUS 204 TO RETURN', async() => {
+      await carController.delete(request, response);
+      expect((response.status as sinon.SinonStub).calledWith(204)).to.be.equal(true);
+    });
+  });
+});
---
+++ src/tests/unit/mocks/mocks.ts
@@ -0,0 +1,74 @@
+export const carExemplo = {
+  model: 'Celta Preto',
+  year: 1993,
+  color: 'red',
+  status: true,
+  buyValue: 54399,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const carExemploInvalido = {
+  model: 'Ford',
+  year: 1993,
+  color: 'red',
+  status: true,
+  buyValue: 54399,
+  doorsQty: '4',
+  seatsQty: 5,
+};
+
+export const carExemploAtualizada = {
+  model: 'Ford',
+  year: 1993,
+  color: 'Black',
+  status: false,
+  buyValue: 54399,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const carResposta = {
+  _id: '5e9f8f9f9f9f9f9f9f9f9f92',
+  model: 'Ferrari',
+  year: 1993,
+  color: 'Black',
+  status: false,
+  buyValue: 54399,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const carsListaResposta = [
+  {
+    _id: '5e9f8f9f9f9f9f9f9f9f9f92',
+    model: 'Ford',
+    year: 1993,
+    color: 'red',
+    status: true,
+    buyValue: 54399,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    _id: '5e9f8f9f9f9f9f9f9f9f9f93',
+    model: 'Fiat Uno',
+    year: 2000,
+    color: 'white',
+    status: true,
+    buyValue: 24999,
+    doorsQty: 4,
+    seatsQty: 5,
+  }
+];
+
+export const carRespostaAtualizada = {
+  _id: '5e9f8f9f9f9f9f9f9f9f9f92',
+  model: 'Ford',
+  year: 1993,
+  color: 'red',
+  status: true,
+  buyValue: 54399,
+  doorsQty: 4,
+  seatsQty: 5,
+};
